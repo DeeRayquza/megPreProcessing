6b42698feb99ff027986e9062c55e27e896b95b6
DeeRayquza <52299512+DeeRayquza@users.noreply.github.com>
2023-08-16 13:39:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ sensor_info = pd.read_excel(sensor_info_path, usecols=[0, 1])  # Read only the f
 # Filter out empty rows
 sensor_info.dropna(subset=['right sensors', 'left sensors'], inplace=True)
 
-# Sort the DataFrame by 'left sensors' column
-sensor_info.sort_values('left sensors', inplace=True)
-
 # Read left occipital file
 left_tfr = pd.read_csv(left_sensor_path)
 
@@ -32,10 +29,10 @@ left_tfr = left_tfr[left_tfr['ch_type'] == 'grad']
 right_tfr = right_tfr[right_tfr['ch_type'] == 'grad']
 
 # Drop all columns except 'channel' and 'value' from left_tfr DataFrame
-left_tfr = left_tfr[['channel', 'value']]
+left_tfr = left_tfr[['channel', 'value', 'freq', 'time']]
 
 # Drop all columns except 'channel' and 'value' from right_tfr DataFrame
-right_tfr = right_tfr[['channel', 'value']]
+right_tfr = right_tfr[['channel', 'value', 'freq', 'time']]
 
 # Remove 'MEG' and convert 'channel' values to float64 in left_tfr DataFrame
 left_tfr['channel'] = left_tfr['channel'].str.replace('MEG', '').astype('float64')
@@ -50,7 +47,7 @@ right_tfr.reset_index(drop=True, inplace=True)
 individual_channels = len(left_tfr) // len(sensor_info)
 
 # Create an empty DataFrame
-result_df = pd.DataFrame(columns=['Left Sensor Label', 'Left Value', 'Right Sensor Label', 'Right Value'])
+result_df = pd.DataFrame(columns=['Left Sensor Label', 'Left Value', 'Right Sensor Label', 'Right Value', 'freq', 'time'])
 
 # Set the countdown variable
 countdown = len(sensor_info)
@@ -62,23 +59,33 @@ for index, row in sensor_info.iterrows():
 
     # Find the index of the matching channel in left_tfr and right_tfr
     left_index = left_tfr[left_tfr['channel'] == left_sensor].index[0]
+
+    # Find the corresponding channel in right_tfr
     right_index = right_tfr[right_tfr['channel'] == right_sensor].index[0]
 
-    # Select channels and values from left_tfr using iloc
+    # Select channels, values, freq, and time from left_tfr using iloc
     left_selected = left_tfr.iloc[left_index:left_index + individual_channels,
-                    [0, 1]]  # Assuming column positions are 0 (channel) and 1 (value)
+                    [0, 1, 2, 3]]  # Assuming column positions are 0 (channel), 1 (value), 2 (freq), and 3 (time)
 
-    # Select channels and values from right_tfr using iloc
+    # Select channels, values, freq, and time from right_tfr using iloc
     right_selected = right_tfr.iloc[right_index:right_index + individual_channels,
-                     [0, 1]]  # Assuming column positions are 0 (channel) and 1 (value)
+                     [0, 1, 2, 3]]  # Assuming column positions are 0 (channel), 1 (value), 2 (freq), and 3 (time)
 
     # Append to result_df
     for i in range(individual_channels):
+        left_label = left_selected.iloc[i]['channel']
+        left_val = left_selected.iloc[i]['value']
+        right_label = right_selected.iloc[i]['channel']
+        right_val = right_selected.iloc[i]['value']
+        f = left_selected.iloc[i]['freq']
+        t = left_selected.iloc[i]['time']
         result_df = result_df._append({
             'Left Sensor Label': left_selected.iloc[i]['channel'],
             'Left Value': left_selected.iloc[i]['value'],
             'Right Sensor Label': right_selected.iloc[i]['channel'],
-            'Right Value': right_selected.iloc[i]['value']
+            'Right Value': right_selected.iloc[i]['value'],
+            'freq': left_selected.iloc[i]['freq'],  # Assuming freq is the same for left and right
+            'time': left_selected.iloc[i]['time']   # Assuming time is the same for left and right
         }, ignore_index=True)
 
     # Decrease the countdown
@@ -87,9 +94,10 @@ for index, row in sensor_info.iterrows():
     # Print the countdown value
     print(f"Iteration {countdown} remaining")
 
+print("Dataframe Successful")
+
 # Add a new column to result_df
-result_df['Calculation'] = (result_df['Right Value'] - result_df['Left Value']) / (
-            result_df['Right Value'] + result_df['Left Value'])
+result_df['Calculation'] = (result_df['Right Value'] - result_df['Left Value']) / (result_df['Right Value'] + result_df['Left Value'])
 
 # Calculate the average of 'Calculation' column
 average_calculation = result_df['Calculation'].mean()
@@ -98,4 +106,4 @@ average_calculation = result_df['Calculation'].mean()
 result_df['Average Calculation'] = average_calculation
 
 # Export the DataFrame to a CSV file
-result_df.to_csv('result.csv', index=False)  # You can replace the result.csv with the output path variable
+result_df.to_csv('result.csv', index=False)
